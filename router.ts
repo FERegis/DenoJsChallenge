2f35157c15133b05ed284f2eb7e64a658b19137d
FERegis <filipeeduardoregis2017@gmail.com>
2020-06-10 21:41:22 -0300

    Generic Add

--- router.ts
+++ router.ts
@@ -1,35 +1,21 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import { camelCase, constantCase, dotCase, headerCase, lowerCase, lowerFirstCase, normalCase, pathCase, sentenceCase, snakeCase, swapCase, titleCase, upperCase, upperFirstCase  } from "https://deno.land/x/case/mod.ts";
+import * as lol from "https://deno.land/x/case/mod.ts";
 
 const router = new Router();
 
     const resolveWord = ({params, response}: {params: {word : string}; response: any},) => {
-        var body = {}
-    if(params.word){
-        response.status = 200;
-        body = {
-        Word : params.word,
-        CamelCase : camelCase(params.word),
-        ConstantCase : constantCase(params.word),
-        DotCase : dotCase(params.word),
-        HeaderCase : headerCase(params.word),
-        LowerCase : lowerCase(params.word),
-        LowerFirstCase : lowerFirstCase(params.word),
-        NormalCase : normalCase(params.word),
-        PathCase : pathCase(params.word),
-        SenteceCase : sentenceCase(params.word),
-        SnakeCase : snakeCase(params.word),
-        SwapCase : swapCase(params.word),
-        TitleCase : titleCase(params.word),
-        UpperCase : upperCase(params.word),
-        UpperFirstCase: upperFirstCase(params.word)
-        }
-    } else {
-        response.status = 400;   
+    const objetos = Object.entries(lol)
+    const body = []
+
+    for (let index = 0; index < objetos.length; index++) {
+        const func = objetos[index][1];
+        const words = {Tipo : objetos[index][0], Resolvido : func(params.word)}
+        body.push(words)
     }
+
     response.body = body;
 }
 
-router.post("/:word", resolveWord)
+router.get("/:word", resolveWord)
 
 export default router;
